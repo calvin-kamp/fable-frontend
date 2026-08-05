029994d671b2426a409f88621267c352e1c5ff11
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-08-05 16:29:36 +0200

    chore: initialalize project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('fable-frontend');
+    protected readonly title = signal('fable-frontend')
 }
