003a3d8939f20c103ae1d9917ab946af98c1faa0
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-08-06 16:14:37 +0200

    feat(components): add ui-badge component

---
+++ src/app/shared/components/badge/badge.ts
@@ -0,0 +1,15 @@
+import { Component, computed, input } from '@angular/core'
+
+type BadgeType = 'primary' | 'success' | 'warning' | 'danger'
+
+@Component({
+    selector: 'ui-badge',
+    imports: [],
+    templateUrl: './badge.html',
+    styleUrl: './badge.scss',
+})
+export class Badge {
+    readonly type = input<BadgeType>()
+
+    protected readonly modifierClass = computed((): string => `badge--${this.type()}`)
+}
